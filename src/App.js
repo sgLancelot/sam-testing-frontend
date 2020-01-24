dddf5e1bf9b1ab1320d2a5a000c79a8408eefe44
tzhenkai <sglancelot@gmail.com>
2020-01-24 17:08:48 +0800

    Installed axios, semantic-ui, db.json and tested onSubmit service.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const FormExampleForm = () => (
+  <Form onSubmit={handleAddGuest}>
+    <Form.Field>
+      <label>Name</label>
+      <input placeholder='Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>RSVP</label>
+      <input placeholder='RSVP' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
+
+const handleAddGuest = async (event) => {
+  event.preventDefault()
+  try {
+    await axios.post('/guests', {"id": 1, "body": "some name"})
+    console.log('successfully added guest!')
+  } catch(exception) {
+    console.log('error adding guets!')
+  }
 }
 
-export default App;
+const App = () => {
+  return (
+    <FormExampleForm />
+  )
+}
+
+export default App
